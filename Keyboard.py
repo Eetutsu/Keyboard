9b74186f9b77063cc0423a58fc215ca1764f4e0b
Eetutsu <eetuotto02@gmail.com>
2024-03-01 10:33:11 +0200

    lisättiin nappeja ja muokatiin väriä

--- Keyboard.py
+++ Keyboard.py
@@ -2,9 +2,13 @@ import tkinter as tk
 from winsound import *
 from playsound import playsound
 
-playA0 = lambda: playsound('sounds\piano-mp3_A0.mp3')
-playA1 = lambda: playsound('sounds\piano-mp3_A1.mp3')
-whiteKeys = [("A0",playA0),("A1",playA1)]
+playC1 = lambda: playsound('sounds\piano-mp3_C1.mp3')
+playD1 = lambda: playsound('sounds\piano-mp3_D1.mp3')
+playE1 = lambda: playsound('sounds\piano-mp3_E1.mp3')
+playDb1 = lambda: playsound('sounds\piano-mp3_Db1.mp3')
+playEb1 = lambda: playsound('sounds\piano-mp3_Eb1.mp3')
+whiteKeys = [("C1",playC1),("D1",playD1),("E1",playE1)]
+blackKeys = [("Db1",playDb1),("Eb1",playEb1)]
 window = tk.Tk()
 window.geometry("500x500")
 greeting = tk.Label(text="Tervetukia kosketinsoittimeen")
@@ -12,8 +16,13 @@ greeting.pack()
 x_pos = 0
 y_pos = 100
 for key in whiteKeys:
-    x_pos = x_pos+25
-    tk.Button(window, text = key[0], command = key[1]).place(x=x_pos,y=y_pos)   
+    x_pos = x_pos+50
+    tk.Button(window, text = key[0], command = key[1], width=5,background="white").place(x=x_pos,y=y_pos)   
+x_pos = 25
+y_pos = 75
+for key in blackKeys:
+    x_pos = x_pos+50
+    tk.Button(window, text = key[0], command = key[1], background="black").place(x=x_pos,y=y_pos)   
 
 
 
